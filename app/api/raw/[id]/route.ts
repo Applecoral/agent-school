3393e5cbe9b602cfb1db8bda7bb3f9f0b135edc4
Apple Coraline <infernoalgae@gmail.com>
2026-03-07 04:43:35 +0100

    [CommitFlow] Fix: Syntax error in Route Handler and setting static delivery

--- app/api/raw/[id]/route.ts
+++ app/api/raw/[id]/route.ts
@@ -1,9 +1,11 @@
 import { getResourceById, getResources } from '@/lib/resources';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-static';
+
 export async function generateStaticParams() {
   const resources = await getResources();
-  return resources.map((resource) => ({
+  return resources.map((resource) => {
     id: resource.id,
   }));
 }
@@ -11,7 +13,7 @@ export async function generateStaticParams() {
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
-] {
+) {
   const { id } = params;
   const resource = await getResourceById(id);
 
